0fdebddd40f0762b092c23bed2f590506176b542
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-09-28 20:01:14 +0900

    edit Create.vue

--- app/components/Classes.vue
+++ app/components/Classes.vue
@@ -2,14 +2,15 @@
         <DockLayout stretchLastChild="true" height="17%">
             <Label dock="left" width="5%" :backgroundColor="classes.color"/>
             <StackLayout class="classes">
-                <Label textWrap="true" class="title" :text="classes.title" />
+                <Label textWrap="true" class="title" :text="classes.name" />
                 <Label textWrap="true" class="room" :text="classes.room" />
+                <!-- <Label textWrap="true" class="room" :text="classes.absent" /> -->
             </StackLayout>
         </DockLayout>
 </template>
  <script>
 export default {
-  props: ['classes']
+  props: ["classes"]
 };
 </script>
 <style scoped>
--- app/components/Create.vue
+++ app/components/Create.vue
@@ -9,36 +9,54 @@
     <StackLayout>
         <DockLayout stretchLastChild="true" >
           <Label text="科目名" dock="left" width="100"  verticalAlignment="center" />
-          <TextField :text="textFieldValue" />
+          <TextField :text="name" />
         </DockLayout>
         <DockLayout stretchLastChild="true" >
           <Label text="教室" dock="left" width="100" verticalAlignment="center" />
-          <TextField :text="textFieldValue" />
+          <TextField :text="room" />
         </DockLayout>
         <DockLayout stretchLastChild="true" >
           <Label text="教員" dock="left" width="100" verticalAlignment="center"  />
-          <TextField :text="textFieldValue" />
+          <TextField :text="teacher" />
         </DockLayout>
         <DockLayout stretchLastChild="true" >
           <Label text="休める回数" dock="left" width="100" verticalAlignment="center"  />
-          <TextField :text="textFieldValue" keyboardType="number" />
+          <TextField :text="absentCount" keyboardType="number" />
         </DockLayout>
         <DockLayout stretchLastChild="true" >
           <Label text="補足" dock="left" width="100" verticalAlignment="center"  />
-          <TextView  :text="textFieldValue" />
+          <TextView  :text="memo" />
+        </DockLayout>
+        <DockLayout stretchLastChild="true" height="17%">
+            <Label dock="left" width="100" text="サンプル" verticalAlignment="center" />
+            <Label class="colorLabel" dock="left" width="5%" :backgroundColor="color"/>
+            <StackLayout class="classes">
+                <Label textWrap="true" class="name" text="アーキテクチャ" />
+                <Label textWrap="true" class="room" text="3105" />
+            </StackLayout>
         </DockLayout>
         <DockLayout stretchLastChild="true">
-          <Label text="ラベルカラー" dock="left" width="100" verticalAlignment="center"  />
-          <WrapLayout  v-for="color in colorList" :key="color">
-        
-            <CheckBox :fillColor="color" :value="color" @tap="onItemTap"/>
+          <Label text="ラベルカラー" dock="left" width="100" verticalAlignment="center" />
+          <WrapLayout >
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#DDDDDD" @tap="selectColor('#DDDDDD')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#00caab" @tap="selectColor('#00caab')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#3d5afe" @tap="selectColor('#3d5afe')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#795548" @tap="selectColor('#795548')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#006968" @tap="selectColor('#006968')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#5c687c" @tap="selectColor('#5c687c')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#8120ff" @tap="selectColor('#8120ff')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#ffea00" @tap="selectColor('#ffea00')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#aee406" @tap="selectColor('#aee406')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#f57c00" @tap="selectColor('#f57c00')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#ff1744" @tap="selectColor('#ff1744')"/>
+            <Label class="colorLabel" width="20" height="20" backgroundColor="#30bcff" @tap="selectColor('#30bcff')"/>
           </WrapLayout >
         </DockLayout>
     </StackLayout>
 	</Page>
 </template>
 <script>
-import Subjects from './Subjects';
+import Subjects from "./Subjects";
 
 export default {
   data() {
@@ -46,25 +64,17 @@ export default {
       Subjects: Subjects,
       slctColor: true,
       myCheckedProp: false,
-      colorList: [
-        '#DDDDDD',
-        '#00caab',
-        '#3d5afe',
-        '#795548',
-        '#006968',
-        '#5c687c',
-        '#8130ff',
-        '#ffea00',
-        '#aee406',
-        '#f57c00',
-        '#ff1744',
-        '#30bcff'
-      ]
+      name: "",
+      room: "",
+      color: "#30bcff",
+      teacher: "",
+      absentCount: "",
+      memo: ""
     };
   },
   methods: {
-    onItemTap(event) {
-      alert(event.toString());
+    selectColor(color) {
+      this.color = color;
     }
   },
   components: {
@@ -83,5 +93,18 @@ ActionBar {
 }
 DockLayout {
   padding: 10 40;
+}
+.colorLabel {
+  margin: 5;
+}
+.classes {
+  text-align: center;
+  vertical-align: center;
+}
+.title {
+  font-size: 13%;
+}
+.room {
+  font-size: 11%;
 }
 </style>
